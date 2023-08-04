60afbf2c3503b114389a434c2402ae9338ae5857
Felipe Valencia <felipe.valencia.2003@gmail.com>
2023-08-04 13:49:55 -0500

    Floyd's Tortoise and Hare

---
+++ 287.find-the-duplicate-number.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=287 lang=python3
+#
+# [287] Find the Duplicate Number
+#
+
+# @lc code=start
+class Solution:
+    def findDuplicate(self, nums: list[int]) -> int:
+        # Floyd's Tortoise and Hare (Cycle Detection)
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        tortoise = nums[0]
+        hare = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+        ptr1 = nums[0]
+        ptr2 = tortoise
+        while ptr1 != ptr2:
+            ptr1 = nums[ptr1]
+            ptr2 = nums[ptr2]
+        return ptr1
+
+# @lc code=end
